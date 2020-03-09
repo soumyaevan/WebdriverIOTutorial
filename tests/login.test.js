7e7f7428e2627d10be30dbe651d115124e84fda7
Soumya sen <sensoumya94@gmail.com>
2020-03-09 23:58:42 +0100

    added jenkis, allure and framework functionality

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,6 +2,7 @@ import {expect} from 'chai';
 import HomePage from '../Pages/HomePage';
 import LoginPage from '../Pages/LoginPage';
 const localConfig = require('../lib/test.config');
+
 describe('Login test of webappsecurity page', () => {
     it('Validate Home page and navigate to Login Page',()=>{
         HomePage.open();
