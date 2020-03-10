9acea92fa599b864f7a97e38f5ce8d27294f8f91
Soumya sen <sensoumya94@gmail.com>
2020-03-10 16:30:13 +0100

    Added API testing

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,6 +1,7 @@
 import {expect} from 'chai';
 import HomePage from '../Pages/HomePage';
 import LoginPage from '../Pages/LoginPage';
+const axios = require('axios');
 const localConfig = require('../lib/test.config');
 
 describe('Login test of webappsecurity page', () => {
@@ -15,4 +16,33 @@ describe('Login test of webappsecurity page', () => {
         LoginPage.submitLoginCredential(localConfig.username,localConfig.password);
         LoginPage.validateErrorMsg(localConfig.errorMessage);
     })
+
+    it('Post request uning axios', async () =>{
+        const params = {
+            projectIds: [345],
+            tagVersion: "66be63ff2ade1c794c0cb24bbc25157fff4ad186"
+        }
+        let res = await axios.post('https://dcb-staging.eu-west-1.csq.io/tag-builder/build',params);
+        console.log(res.data);
+        let responseToValidate = res.data.success;
+        expect(responseToValidate).to.be.equal(true);
+    })
+
+    it('PUT request using axios', async () => {
+        const params = {
+            autoInsightsEnabled : true
+        };
+        const headers = {
+            'Content-Type': 'application/json'
+        }
+        let res = await axios.put('https://projects-staging.csq.io/v1/tags/345',params,headers);
+        console.log(res.data);
+        let dataToValidate = res.data.payload.autoInsightsEnabled;
+        let responseToValidate = res.data.success;
+        console.log(dataToValidate);
+        console.log(responseToValidate);
+        expect(dataToValidate).to.be.equal(true);
+        expect(responseToValidate).to.be.equal(true);
+    })
+
 })
--- wdio.conf.js
+++ wdio.conf.js
@@ -126,7 +126,7 @@ exports.config = {
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
     // reporters: ['spec'],
-    reporters: [['allure', {
+    reporters: ['spec',['allure', {
         outputDir: 'allure-results',
         disableWebdriverStepsReporting: true,
         disableWebdriverScreenshotsReporting: true,
